ae6e3aaef3755e88030edb80670626a7c78cb96f
tenzan <xtenzanxt1@gmail.com>
2021-10-07 00:04:20 +0800

    Add project files.

---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ITokenService _tokenService;
+
+        public WeatherForecastController(ITokenService tokenService, ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _tokenService = tokenService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("gettoken")]
+        public async Task<string> GetToken()
+        {
+            var tokenResponse = await _tokenService.GetToken("weatherapi.read");
+
+            return tokenResponse.AccessToken;
+        }
+    }
+}
